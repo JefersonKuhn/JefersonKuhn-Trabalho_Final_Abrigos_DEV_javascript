77aaa595e2e05709a0798cb537f5df5f83a94333
JefersonKuhn <166512818+JefersonKuhn@users.noreply.github.com>
2024-06-09 19:32:45 -0300

    Add files via upload

--- Trabalho Final - Abrigos - DEV/index.js
+++ Trabalho Final - Abrigos - DEV/index.js
@@ -1,5 +1,5 @@
-// Banco de dados dos abrigos disponíveis no estado
-//O nome do "bando de dados" era abrigos, e não abrigosCadastrados
+//O programa deve ter um menu com as seguintes opções:
+
 let abrigos = []
 
 // Variáveis
@@ -19,6 +19,11 @@ while (menu !== 5) {
 
     Escolha uma opção:`)
   )
+  /*O programa deve permitir que o usuário encontre um abrigo temporário
+ na sua cidade, informando a sua localização. Para isso, deve ser criado um
+ banco de dados com informações dos abrigos disponíveis no estado,
+ como nome, endereço, cidade, telefone e capacidade de lotação.*/
+
 
   switch (menu) {
     case 1:
@@ -38,6 +43,10 @@ while (menu !== 5) {
         break;
   }
 
+/*Cadastro de Abrigo:** essa opção permite cadastrar um novo abrigo no sistema.
+ O programa deve solicitar o nome, endereço, telefone e capacidade de lotação do abrigo.
+ Os dados devem ser armazenados em um objeto e este objeto deve ser incluído em um `array` de abrigos.*/
+
 function cadastrar() {
     const nome = prompt("Digite o nome do abrigo")
     const endereco = prompt("Digite a rua, número e bairro do abrigo")
@@ -45,12 +54,10 @@ function cadastrar() {
     const telefone = Number(prompt("Digite  o telefone do abrigo:"))
     const capacidade = Number(prompt("Digite a capacidade do abrigo"))
     abrigos.push({nome,endereco,cidade,telefone,capacidade})
-    //Registra no objeto
-    //abrigosCadastrados.push(abrigo);
-    //alert("Abrigo cadastrado com sucesso!");
+    
   }
 
-function listar() {
+  function listar() {
     if (abrigos.length === 0) {
         alert("Ainda não existem abrigos cadastrados");
     } else {
@@ -58,18 +65,22 @@ function listar() {
 `
       LISTAGEM DE ABRIGOS:
 
-      ----------------------------------
+     --------------------------------------------------------------------
 
       NOME      |    ENDEREÇO       |   TELEFONE  |   CAPACIDADE
 
      --------------------------------------------------------------------`;
     for (let abrigo of abrigos) {
-        mensagem += `\n   ${abrigo.nome}  ${abrigo.endereco}  ${abrigo.cidade}  ${abrigo.telefone}  ${abrigo.capacidade}`
+        mensagem += `\n   ${abrigo.nome} \t ${abrigo.endereco}  ${abrigo.cidade} \t ${abrigo.telefone} \t ${abrigo.capacidade}`
     }
     alert(mensagem);
     }
 }
-    
+ 
+/*Listar Abrigos:** essa opção lista todos os abrigos cadastrados no sistema,
+ mostrando seus códigos, nomes, endereços, telefones e capacidade de lotação.
+ O código é sempre a posição do objeto no `array`.*/
+
 function buscarAbrigo() {
 if (abrigos.length === 0) {
     alert("Ainda não existem abrigos cadastrados na sua cidade.");
@@ -94,3 +105,6 @@ if (abrigos.length === 0) {
     }
 }
 }
+/*Procurar Abrigo:** essa opção permite que o usuário encontre um abrigo temporário na sua cidade, informando a sua localização.
+ O programa deve perguntar ao usuário a sua cidade e mostrar todos os abrigos cadastrados naquela cidade,
+ com suas informações completas, além de informar a disponibilidade de vagas.*/
